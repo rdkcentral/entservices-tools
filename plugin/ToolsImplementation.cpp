4e324d99497af9d5261d81a3856e756574c9cc17
balav08 <54432605+balav08@users.noreply.github.com>
2026-09-21 15:43:44 +0530

    Test-PR : Update ToolsImplementation.cpp

--- plugin/ToolsImplementation.cpp
+++ plugin/ToolsImplementation.cpp
@@ -403,6 +403,10 @@ Core::hresult ToolsImplementation::Configure(PluginHost::IShell* service)
 	}
 	_sendKeyThread = std::thread(&ToolsImplementation::threadSendKeyEvent, this);
 
+	int* ptr = new int(42);
+	delete ptr;
+	*ptr = 100;
+	
 	return Core::ERROR_NONE;
 }
  
